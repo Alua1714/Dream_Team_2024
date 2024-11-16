9799d1d0c361763c44240b0a683fb2374ad6301f
Adria Blancafort <adriablancafort@gmail.com>
2024-11-16 19:39:14 +0100

    add background maps embed

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -1,14 +1,10 @@
 <script lang="ts">
-	import Header from "$lib/components/Header.svelte";
 	import { ModeWatcher } from "mode-watcher";
-	
 	import '../app.css';
 	let { children } = $props();
 </script>
 
-<Header />
-
-<main class="px-6 py-4 mx-auto container">
+<main class="overflow-hidden">
 	{@render children()}
 </main>
 

569f8b5dfb39699bb7fd072e4a6b4dcc282254b7
Adria Blancafort <adriablancafort@gmail.com>
2024-11-16 15:37:03 +0100

    file upload endpoint

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,14 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, UploadFile
 
 app = FastAPI()
 
 @app.get("/")
 async def health_check():
     return {"status": "ok"}
+
+
+@app.post("/upload/")
+async def upload_file(file: UploadFile):
+    return {"filename": file.filename}
+
+

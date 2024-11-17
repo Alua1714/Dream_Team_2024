472f4dbfc3533c25b6239ac1d7fbe8b7c6d951f7
Adria Blancafort <adriablancafort@gmail.com>
2024-11-17 04:44:13 +0100

    side panel basic functionality

--- frontend/src/lib/components/Sidepanel.svelte
+++ frontend/src/lib/components/Sidepanel.svelte
@@ -1,33 +1,100 @@
 <script lang="ts">
   import { Button } from "$lib/components/ui/button/index.js";
+  import { Label } from "$lib/components/ui/label/index.js";
+  import { LoaderCircle } from "lucide-svelte";
   import { ScrollArea } from "$lib/components/ui/scroll-area/index.js";
-  import { Skeleton } from "$lib/components/ui/skeleton/index.js";
 
   import * as Card from "$lib/components/ui/card/index.js";
 
   import { map } from "$lib/state/map.svelte";
 
-  let lon = $state(43.4321);
-  let lat = $state(43.4532);
+  let files = $state(null);
+  let formLoading = $state(false);
+  let results = $state(null);
 
-  function selectLocation() {
-    map.setCoordinates(lon, lat);
+  async function handleSubmit() {
+    formLoading = true;
+    try {
+      const formData = new FormData();
+      formData.append('file', files[0]);
+
+      const response = await fetch(`${import.meta.env.VITE_API_ENDPOINT}/upload/`, {
+        method: 'POST',
+        headers: {
+          'accept': 'application/json'
+        },
+        body: formData
+      });
+
+      const data = await response.json();
+      results = data;
+      // success
+    } catch (error) {
+      // error
+    } finally {
+      formLoading = false;
+    }
   }
 
-  const skeletons = new Array(20).fill(0);
+  function resetResults() {
+    results = null;
+    files = null;
+  }
+
+  function selectLocation(house) {
+    map.setCoordinates(house.location.latitude, house.location.longitude);
+    console.log("Go to coordinates", house.location.latitude, house.location.longitude);
+  }
 </script>
 
-<Card.Root class="absolute top-2 left-2 bottom-16 w-96 z-10">
-  <div class="py-6 px-2">
-    <ScrollArea class="h-[545px] rounded">
-        <div class="space-y-3 px-4">
-          {#each skeletons as skeleton}
-            <Skeleton class="h-20 rounded w-full" />
-          {/each}
-        </div>
+<Card.Root class="absolute top-2 left-2 bottom-16 w-96 z-10">´
+{#if results}
+  <div class="px-2 pb-6">
+    <ScrollArea class="h-[500px] rounded">
+      <div class="space-y-3 px-4 w-full">
+        {#each results as house}
+          {@render houseCard(house)}
+        {/each}
+      </div>
     </ScrollArea>
   </div>
-  <Card.Footer class="mt-auto">
-    <Button class="w-full" onclick={selectLocation}>Upload Another file</Button>
+  <Card.Footer>
+    <Button class="w-full" onclick={resetResults}>Upload Another file</Button>
   </Card.Footer>
+{:else}
+  <form onsubmit={handleSubmit}>
+    <Card.Header>
+      <Card.Title>Import new dataset</Card.Title>
+      <Card.Description>
+        Upload the file containing the attributes of the houses you want to predict the price of.
+      </Card.Description>
+    </Card.Header>
+    <Card.Content>
+      <div class="grid w-full items-center gap-4">
+        <div class="flex flex-col space-y-1.5">
+          <Label for="name">Dataset</Label>
+          <input type="file" bind:files required>
+        </div>
+      </div>
+  </Card.Content>
+  <Card.Footer class="flex justify-between">
+    <Button type="submit" class="w-full" disabled={formLoading}>
+      {#if formLoading}
+        <LoaderCircle class="h-5 animate-spin" />
+      {:else}
+        Calculate Prices
+      {/if}
+    </Button>
+  </Card.Footer>
+  </form>
+{/if}
 </Card.Root>
+
+{#snippet houseCard(house)}
+<button onclick={() => selectLocation(house)} class="w-full text-left">
+    <div class="border rounded p-4">
+        <h2>Id: {house.listing_id}</h2>
+        <p>Price: {house.prediction}</p>
+    </div>
+</button>
+{/snippet}

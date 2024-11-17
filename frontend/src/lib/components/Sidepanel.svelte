771c1095a308df6a859ea965cabfbf6b210e1219
Adria Blancafort <adriablancafort@gmail.com>
2024-11-17 08:24:12 +0100

    add sidepanel with house information

--- frontend/src/lib/components/Sidepanel.svelte
+++ frontend/src/lib/components/Sidepanel.svelte
@@ -2,9 +2,10 @@
   import { Button } from "$lib/components/ui/button/index.js";
   import { LoaderCircle } from "lucide-svelte";
   import { ScrollArea } from "$lib/components/ui/scroll-area/index.js";
-  import InfoDialog from "$lib/components/InfoDialog.svelte";
 
   import * as Card from "$lib/components/ui/card/index.js";
+
+  import StreetviewEmbed from "$lib/components/StreetviewEmbed.svelte";
 
   import { map } from "$lib/state/map.svelte";
 
@@ -19,13 +20,16 @@
       const formData = new FormData();
       formData.append("file", files[0]);
 
-      const response = await fetch(`${import.meta.env.VITE_API_ENDPOINT}/upload/`, {
-        method: "POST",
-        headers: {
-          "accept": "application/json"
-        },
-        body: formData
-      });
+      const response = await fetch(
+        `${import.meta.env.VITE_API_ENDPOINT}/upload/`,
+        {
+          method: "POST",
+          headers: {
+            accept: "application/json",
+          },
+          body: formData,
+        }
+      );
 
       const data = await response.json();
       results = data;
@@ -49,65 +53,92 @@
 </script>
 
 <Card.Root class="absolute top-2 left-2 bottom-16 w-96 z-10">
-{#if results}
-  <div class="px-2 py-6">
-    <ScrollArea class="h-[540px] rounded">
-      <div class="space-y-3 px-4 w-full">
-        {#each results as house}
-          {@render houseCard(house)}
-        {/each}
-      </div>
-    </ScrollArea>
-  </div>
-  <Card.Footer>
-    <Button class="w-full" onclick={resetResults}>Upload Another File</Button>
-  </Card.Footer>
-{:else}
-  <form onsubmit={handleSubmit}>
-    <Card.Header>
-      <Card.Title>Import New Dataset</Card.Title>
-      <Card.Description>
-        Upload a file containing the attributes of the houses you want to predict the price of.
-      </Card.Description>
-    </Card.Header>
-    <Card.Content>
-      <div class="grid w-full items-center gap-4">
-          <div class="relative h-[450px] w-full border-2 border-dashed rounded-lg 
-            hover:border-gray-400 transition-colors flex items-center justify-center">
-            <input 
-                type="file" 
-                bind:files 
-                required
-                class="absolute w-full h-full opacity-0 cursor-pointer"
-            />
-            <p class="text-sm">Click to upload or drag and drop</p>
-            </div>
+  {#if results}
+    <div class="px-2 py-6">
+      <ScrollArea class="h-[540px] rounded">
+        <div class="space-y-3 px-4 w-full">
+          {#each results as house}
+            {@render houseCard(house)}
+          {/each}
         </div>
-  </Card.Content>
-  <Card.Footer class="flex justify-between">
-    <Button type="submit" class="w-full" disabled={formLoading}>
-      {#if formLoading}
-        <LoaderCircle class="h-5 animate-spin" />
-      {:else}
-        Calculate Prices
-      {/if}
-    </Button>
-  </Card.Footer>
-  </form>
-{/if}
+      </ScrollArea>
+    </div>
+    <Card.Footer>
+      <Button class="w-full" onclick={resetResults}>Upload Another File</Button>
+    </Card.Footer>
+  {:else}
+    <form onsubmit={handleSubmit}>
+      <Card.Header>
+        <Card.Title>Import New Dataset</Card.Title>
+        <Card.Description>
+          Upload a file containing the attributes of the houses you want to
+          predict the price of.
+        </Card.Description>
+      </Card.Header>
+      <Card.Content>
+        <div class="grid w-full items-center gap-4">
+          <div
+            class="relative h-[450px] w-full border-2 border-dashed rounded-lg
+            hover:border-gray-400 transition-colors flex items-center justify-center"
+          >
+            <input
+              type="file"
+              bind:files
+              required
+              class="absolute w-full h-full opacity-0 cursor-pointer"
+            />
+            {#if files?.[0]}
+              <p class="text-sm font-medium">{files[0].name}</p>
+            {:else}
+              <p class="text-sm">Click to upload or drag and drop</p>
+            {/if}
+          </div>
+        </div>
+      </Card.Content>
+      <Card.Footer class="flex justify-between">
+        <Button type="submit" class="w-full" disabled={formLoading}>
+          {#if formLoading}
+            <LoaderCircle class="h-5 animate-spin" />
+          {:else}
+            Calculate Prices
+          {/if}
+        </Button>
+      </Card.Footer>
+    </form>
+  {/if}
 </Card.Root>
 
 {#snippet houseCard(house)}
-<button 
-  onclick={() => selectLocation(house)} 
-  class="w-full text-left rounded p-4 transition-all relative {selectedHouse?.listing_id === house.listing_id ? "border-2 border-primary" : "border"} "
->
-  <div>
-    <h2>Id: {house.listing_id}</h2>
-    <p>Price: {house.prediction}</p>
-    <div class="absolute right-3 top-3">
-        <InfoDialog house={house} />
+  <button
+    onclick={() => selectLocation(house)}
+    class="w-full text-left rounded p-4 transition-all relative {selectedHouse?.listing_id ===
+    house.listing_id
+      ? 'border-2 border-primary'
+      : 'border'} "
+  >
+    <div>
+      <h2>Id: {house.listing_id}</h2>
+      <p>Price: {house.prediction}</p>
     </div>
-  </div>
-</button>
+  </button>
 {/snippet}
+
+{#if selectedHouse}
+  <Card.Root
+    class="absolute right-2 bottom-2 z-10 {selectedHouse === null
+      ? 'hiden'
+      : ''}"
+  >
+    <StreetviewEmbed />
+    <Card.Header>
+      <Card.Title>Import New Dataset</Card.Title>
+      <Card.Description>
+        Upload a file containing the attributes of the houses
+      </Card.Description>
+    </Card.Header>
+    <Card.Content>
+      <p>Selected house: {selectedHouse?.listing_id}</p>
+      <p>Price: {selectedHouse?.prediction}</p>
+    </Card.Content>
+  </Card.Root>
+{/if}
---
+++ frontend/src/lib/components/StreetviewEmbed.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  import { map } from "$lib/state/map.svelte";
+  const googleMapsApiKey = import.meta.env.VITE_GOOGLE_MAPS_API_KEY;
+</script>
+
+<div class="h-80 w-full">
+  <iframe
+    title="Google Street View"
+    class="h-full w-full border-0 dark:invert-[90%]"
+    src={`https://www.google.com/maps/embed/v1/streetview?key=${googleMapsApiKey}&location=${map.coordinates}&heading=210&pitch=10&fov=90`}
+  ></iframe>
+</div>

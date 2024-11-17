6c67d7207d30d6f60d8e4acbba00401f4497a14d
Adria Blancafort <adriablancafort@gmail.com>
2024-11-17 05:17:56 +0100

    feat: click file upload

--- frontend/src/lib/components/Sidepanel.svelte
+++ frontend/src/lib/components/Sidepanel.svelte
@@ -47,10 +47,10 @@
   }
 </script>
 
-<Card.Root class="absolute top-2 left-2 bottom-16 w-96 z-10">´
+<Card.Root class="absolute top-2 left-2 bottom-16 w-96 z-10">
 {#if results}
-  <div class="px-2 pb-6">
-    <ScrollArea class="h-[500px] rounded">
+  <div class="px-2 py-6">
+    <ScrollArea class="h-[540px] rounded">
       <div class="space-y-3 px-4 w-full">
         {#each results as house}
           {@render houseCard(house)}
@@ -64,18 +64,24 @@
 {:else}
   <form onsubmit={handleSubmit}>
     <Card.Header>
-      <Card.Title>Import new dataset</Card.Title>
+      <Card.Title>Import New Dataset</Card.Title>
       <Card.Description>
-        Upload the file containing the attributes of the houses you want to predict the price of.
+        Upload a file containing the attributes of the houses you want to predict the price of.
       </Card.Description>
     </Card.Header>
     <Card.Content>
       <div class="grid w-full items-center gap-4">
-        <div class="flex flex-col space-y-1.5">
-          <Label for="name">Dataset</Label>
-          <input type="file" bind:files required>
+          <div class="relative h-[450px] w-full border-2 border-dashed rounded-lg 
+            hover:border-gray-400 transition-colors flex items-center justify-center">
+            <input 
+                type="file" 
+                bind:files 
+                required
+                class="absolute w-full h-full opacity-0 cursor-pointer"
+            />
+            <p class="text-sm">Click to upload or drag and drop</p>
+            </div>
         </div>
-      </div>
   </Card.Content>
   <Card.Footer class="flex justify-between">
     <Button type="submit" class="w-full" disabled={formLoading}>

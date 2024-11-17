95208a047de925b17e14bd63e8b523a427b5be35
Adria Blancafort <adriablancafort@gmail.com>
2024-11-17 03:19:27 +0100

    add skeleton loading

---
+++ frontend/src/lib/components/Sidepanel.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+  import { Button } from "$lib/components/ui/button/index.js";
+  import { ScrollArea } from "$lib/components/ui/scroll-area/index.js";
+  import { Skeleton } from "$lib/components/ui/skeleton/index.js";
+
+  import * as Card from "$lib/components/ui/card/index.js";
+
+  import { map } from "$lib/state/map.svelte";
+
+  let lon = $state(43.4321);
+  let lat = $state(43.4532);
+
+  function selectLocation() {
+    map.setCoordinates(lon, lat);
+  }
+
+  const skeletons = new Array(20).fill(0);
+</script>
+
+<Card.Root class="absolute top-2 left-2 bottom-16 w-96 z-10">
+  <div class="py-6 px-2">
+    <ScrollArea class="h-[545px] rounded">
+        <div class="space-y-3 px-4">
+          {#each skeletons as skeleton}
+            <Skeleton class="h-20 rounded w-full" />
+          {/each}
+        </div>
+    </ScrollArea>
+  </div>
+  <Card.Footer class="mt-auto">
+    <Button class="w-full" onclick={selectLocation}>Upload Another file</Button>
+  </Card.Footer>
+</Card.Root>
